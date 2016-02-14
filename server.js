d29a1422d7180bf13846f71c551a5ebd439c07a6
Jogis <zenjayjay@gmail.com>
2016-02-14 10:58:00 +0800

    add auth

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 "use strict";
+import fs from 'fs';
 import koa from 'koa';
 import convert from 'koa-convert';
 import bodyParser from 'koa-bodyparser';
 import koaStatic from 'koa-static';
+import session from 'koa-session';
+import passport from 'koa-passport';
 import cors from 'koa-cors';
+import route from 'koa-route';
 import _ from 'lodash';
 import path from 'path';
 
+import policy from './lib/middlewares/policy';
 import mock from './lib/middlewares/mock';
 import api from './lib/middlewares/api';
+
+import './lib/strategy';
 
 const app = new koa();
 app.use(convert(cors()));
@@ -19,9 +26,23 @@ app.use(async (ctx, next) => {
   console.log('%s %s - %s', ctx.method, ctx.url, ms);
 })
 
+app.keys = ['json-server'];
+app.use(convert(session(app)));
+app.use(convert(bodyParser()));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(policy);
+
+app.use(async (ctx, next) => {
+  if(ctx.path === '/'){
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream('./lib/client/index.html');
+  }
+  await next();
+})
 const staticPath = path.resolve(__dirname, './lib/client');
 app.use(convert(koaStatic(staticPath)));
-app.use(convert(bodyParser()));
 
 app.use(mock);
 app.use(api);
